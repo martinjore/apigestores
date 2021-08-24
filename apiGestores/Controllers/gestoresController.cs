46400fc73de6f0191d48974ba9533ef5386c47a1
Martinjore <martin_regondi@hotmail.com>
2021-08-24 13:04:55 -0300

    init files

---
+++ apiGestores/Controllers/gestoresController.cs
@@ -0,0 +1,118 @@
+﻿using apiGestores.Context;
+using apiGestores.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace apiGestores.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class gestoresController : ControllerBase
+    {
+        private readonly AppDbContext context;
+        public gestoresController(AppDbContext context)
+        {
+            this.context = context;
+        }
+        // GET: api/<gestoresController>
+        [HttpGet]
+        public ActionResult Get()
+        {
+            try
+            {
+                return Ok(context.Desarrollo.ToList());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+        }
+
+        // GET api/<gestoresController>/5
+        [HttpGet("{id}", Name = "GetGestor")]
+        public ActionResult Get(int id)
+        {
+            try
+            {
+                var gestor = context.Desarrollo.FirstOrDefault(g => g.id == id);
+                return Ok(gestor);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // POST api/<gestoresController>
+        [HttpPost]
+        public ActionResult Post([FromBody] gestoresDB gestor)
+        {
+            try
+            {
+                context.Desarrollo.Add(gestor);
+                context.SaveChanges();
+                return CreatedAtRoute("GetGestor", new { id = gestor.id }, gestor);
+            }
+
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // PUT api/<gestoresController>/5
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, [FromBody] gestoresDB gestor)
+        {
+            try
+            {
+                if (gestor.id == id)
+                {
+                    context.Entry(gestor).State = EntityState.Modified;
+                    context.SaveChanges();
+                    return CreatedAtRoute("GetGestor", new { id = gestor.id }, gestor);
+                }
+                else
+                {
+                    return BadRequest() ;
+                }
+            }
+
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // DELETE api/<gestoresController>/5
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                var gestor = context.Desarrollo.FirstOrDefault(g => g.id == id);
+                if (gestor != null)
+                {
+                    context.Desarrollo.Remove(gestor);
+                    context.SaveChanges();
+                    return Ok(id);
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ apiGestores/Startup.cs
@@ -0,0 +1,73 @@
+using apiGestores.Context;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace apiGestores
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            
+            services.AddControllers();
+            services.AddCors();
+            //services.AddSwaggerGen(c =>
+            //{
+            //    c.SwaggerDoc("v1", new OpenApiInfo { Title = "apiGestores", Version = "v1" });
+            //});
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("conexion")));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseCors(options =>{
+               
+                options.WithOrigins("http://localhost:3000/api/gestores");
+
+                options.AllowAnyMethod();
+
+                options.AllowAnyHeader();
+
+                options.AllowAnyOrigin();
+
+            });
+            if(env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
